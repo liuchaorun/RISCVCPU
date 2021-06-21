6b73fbedd2c0c9ac38f2bf3ad14ce24537b8799f
Xu-SY <1010392329@qq.com>
2021-06-21 21:08:15 +0800

    the third level execution module

--- RISCV/RISCV.srcs/sources_1/new/EX.v
+++ RISCV/RISCV.srcs/sources_1/new/EX.v
@@ -21,6 +21,26 @@
 
 
 module EX(
+        input                   clk,
+        input                   rst,
+        input                   start,
 
+        input       [31:0]      rs1_val,
+        input       [31:0]      rs2_val,
+        input       [31:0]      imm,
+        input       [4:0]       rd_idx,
+        input       [3:0]       op_type,
+        input       [3:0]       alu_type,
+        input                   operand2_sel,
+        input       [31:0]      rd_pc,              // lui, auipc, jal, jalr write to rd
+        input                   rdpc_sel,
+
+        output      [31:0]      rd_val,
+        output      [4:0]       rd_idx,
+        output      [4:0]       op_type_out,
+        output      [31:0]      rs1_val_out,
+        output      [31:0]      imm_out
     );
+
+    
 endmodule
